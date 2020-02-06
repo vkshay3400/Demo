f8ae0f72ff3d3a4d94973a128891dc307c2d6409
vkshay3400 <akshayp2208@gmail.com>
2020-02-06 18:58:17 +0530

    TestingFolder02

---
+++ Testing2/2_NumberandDisplay.sh
@@ -0,0 +1,28 @@
+Sunday# Print number in words
+read -p "Enter number to print 1 - 9: " r
+if (( $r ==1 ))
+then 
+	echo "Sunday"
+elif (( $r ==2 ))	
+then
+	echo "Monday"
+elif (( $r ==3 ))
+then 
+	echo "Tuesday"
+elif (( $r ==4 ))
+then 
+	echo "Wednesday"
+elif (( $r ==5 ))
+then 
+	echo "Thursday"
+elif (( $r ==6 ))
+then 
+	echo "Friday"
+elif (( $r ==7 ))
+then 
+	echo "Saturday"
+ 
+else
+	echo "Pls press between 1-9 for days"
+fi
+
